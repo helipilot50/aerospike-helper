4c1479464499a1e953d4a9b5d7e32026de7c4eed
Peter Milne <helipilot50@gmail.com>
2016-09-29 17:00:14 +0200

    more tests

---
+++ csharp/AerospikeHelper/AerospikeHelper/Tests/DeleterTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using NUnit.Framework;
+using Aerospike.Client;
+using System.Collections.Generic;
+
+namespace Aerospike.Helper.Query
+{
+	public class DeleterTests : HelperTests
+	{
+		public DeleterTests()
+		{
+		}
+
+		[TestCase]
+		public void deleteByKey()
+		{
+			for (int x = 1; x <= TestQueryEngine.RECORD_COUNT; x++)
+			{
+				String keyString = "selector-test:" + x;
+				Key key = new Key(TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, keyString);
+				KeyQualifier kq = new KeyQualifier(Value.Get(keyString));
+				Statement stmt = new Statement();
+				stmt.Namespace = TestQueryEngine.NAMESPACE;
+				stmt.SetName = TestQueryEngine.SET_NAME;
+				IDictionary<string, long> counts = queryEngine.Delete(stmt, kq);
+				Assert.Equals(1L, counts["write"]);
+				Record record = this.client.Get(null, key);
+				Assert.Null(record);
+			}
+		}
+		[TestCase]
+		public void deleteByDigest()
+		{
+			for (int x = 1; x <= TestQueryEngine.RECORD_COUNT; x++)
+			{
+				String keyString = "selector-test:" + x;
+				Key key = new Key(TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, keyString);
+				KeyQualifier kq = new KeyQualifier(key.digest);
+				Statement stmt = new Statement();
+				stmt.Namespace = TestQueryEngine.NAMESPACE;
+				stmt.SetName = TestQueryEngine.SET_NAME;
+				IDictionary<string, long> counts = queryEngine.Delete(stmt, kq);
+				Assert.Equals(1L, counts["write"]);
+				Record record = this.client.Get(null, key);
+				Assert.Null(record);
+			}
+		}
+		[TestCase]
+		public void deleteStartsWith()
+		{
+			Qualifier qual1 = new Qualifier("color", Qualifier.FilterOperation.ENDS_WITH, Value.Get("e"));
+			Statement stmt = new Statement();
+			stmt.Namespace = TestQueryEngine.NAMESPACE;
+			stmt.SetName = TestQueryEngine.SET_NAME;
+			IDictionary<string, long> counts = queryEngine.Delete(stmt, qual1);
+			//System.out.println(counts);
+			//Assert.Equals((Long)400L, (Long)counts.get("read"));
+			//Assert.Equals((Long)400L, (Long)counts.get("write"));
+
+		}
+		[TestCase]
+		public void deleteEndsWith()
+		{
+			Qualifier qual1 = new Qualifier("color", Qualifier.FilterOperation.EQ, Value.Get("blue"));
+			Qualifier qual2 = new Qualifier("name", Qualifier.FilterOperation.START_WITH, Value.Get("na"));
+			Statement stmt = new Statement();
+			stmt.Namespace = TestQueryEngine.NAMESPACE;
+			stmt.SetName = TestQueryEngine.SET_NAME;
+			IDictionary<string, long> counts = queryEngine.Delete(stmt, qual1, qual2);
+			//System.out.println(counts);
+			Assert.Equals(200L, counts["read"]);
+			Assert.Equals(200L, counts["write"]);
+		}
+		[TestCase]
+		public void deleteWithFilter()
+		{
+			Key key = new Key(TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, "first-name-1");
+			Bin firstNameBin = new Bin("first_name", "first-name-1");
+			Bin lastNameBin = new Bin("last_name", "last-name-1");
+			int age = 25;
+			Bin ageBin = new Bin("age", age);
+			this.client.Put(null, key, firstNameBin, lastNameBin, ageBin);
+
+			Qualifier qual1 = new Qualifier("last_name", Qualifier.FilterOperation.EQ, Value.Get("last-name-1"));
+			//DELETE FROM test.people WHERE last_name='last-name-1'
+			Statement stmt = new Statement();
+			stmt.Namespace = TestQueryEngine.NAMESPACE;
+			stmt.SetName = TestQueryEngine.SET_NAME;
+			IDictionary<string, long> counts = queryEngine.Delete(stmt, qual1);
+			Assert.Equals(1L, counts["read"]);
+			Assert.Equals(1L, counts["write"]);
+			Record record = this.client.Get(null, key);
+			Assert.Null(record);
+		}
+
+	}
+}
---
+++ csharp/AerospikeHelper/AerospikeHelper/Tests/SelectorTests.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using NUnit.Framework;
+using Aerospike.Client;
+using System.Collections.Generic;
+namespace Aerospike.Helper.Query
+{
+	public class SelectorTests : HelperTests
+	{
+		public SelectorTests()
+		{
+		}
+		[TestCase]
+		public void selectOneWitKey()
+		{
+			Statement stmt = new Statement();
+			stmt.Namespace = TestQueryEngine.NAMESPACE;
+			stmt.SetName = TestQueryEngine.SET_NAME;
+			KeyQualifier kq = new KeyQualifier(Value.Get("selector-test:3"));
+			KeyRecordEnumerator it = queryEngine.Select(stmt, kq);
+			int count = 0;
+			while (it.MoveNext())
+			{
+				KeyRecord rec = it.Current;
+				count++;
+				//			System.out.println(rec);
+			}
+			it.Close();
+			//		System.out.println(count);
+			Assert.Equals(1, count);
+		}
+
+		[TestCase]
+		public void selectAll()
+		{
+			KeyRecordEnumerator it = queryEngine.Select(TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, null);
+			try
+			{
+				while (it.MoveNext())
+				{
+					KeyRecord rec = it.Current;
+				}
+			}
+			finally
+			{
+				it.Close();
+			}
+		}
+
+		[TestCase]
+		public void selectOnIndex()
+		{
+			IndexTask task = this.client.CreateIndex(null, TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, "age_index", "age", IndexType.NUMERIC);
+			task.Wait();
+			Filter filter = Filter.Range("age", 28, 29);
+			KeyRecordEnumerator it = queryEngine.Select(TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, filter);
+			try
+			{
+				while (it.MoveNext())
+				{
+					KeyRecord rec = it.Current;
+					int age = rec.record.GetInt("age");
+					Assert.True(age >= 28 && age <= 29);
+				}
+			}
+			finally
+			{
+				it.Close();
+			}
+		}
+		[TestCase]
+		public void selectStartsWith()
+		{
+			Qualifier qual1 = new Qualifier("color", Qualifier.FilterOperation.ENDS_WITH, Value.Get("e"));
+			KeyRecordEnumerator it = queryEngine.Select(TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, null, qual1);
+			try
+			{
+				while (it.MoveNext())
+				{
+					KeyRecord rec = it.Current;
+					Assert.True(rec.record.GetString("color").EndsWith("e"));
+
+				}
+			}
+			finally
+			{
+				it.Close();
+			}
+		}
+		[TestCase]
+		public void selectEndsWith()
+		{
+			Qualifier qual1 = new Qualifier("color", Qualifier.FilterOperation.EQ, Value.Get("blue"));
+			Qualifier qual2 = new Qualifier("name", Qualifier.FilterOperation.START_WITH, Value.Get("na"));
+			KeyRecordEnumerator it = queryEngine.Select(TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, null, qual1, qual2);
+			try
+			{
+				while (it.MoveNext())
+				{
+					KeyRecord rec = it.Current;
+					Assert.Equals("blue", rec.record.GetString("color"));
+					Assert.True(rec.record.GetString("name").StartsWith("na"));
+				}
+			}
+			finally
+			{
+				it.Close();
+			}
+		}
+		[TestCase]
+		public void selectOnIndexWithQualifiers()
+		{
+			IndexTask task = this.client.CreateIndex(null, TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, "age_index_selector", "age", IndexType.NUMERIC);
+			task.Wait();
+			Filter filter = Filter.Range("age", 25, 29);
+			Qualifier qual1 = new Qualifier("color", Qualifier.FilterOperation.EQ, Value.Get("blue"));
+			KeyRecordEnumerator it = queryEngine.Select(TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, filter, qual1);
+			try
+			{
+				while (it.MoveNext())
+				{
+					KeyRecord rec = it.Current;
+					Assert.Equals("blue", rec.record.GetString("color"));
+					int age = rec.record.GetInt("age");
+					Assert.True(age >= 25 && age <= 29);
+				}
+			}
+			finally
+			{
+				it.Close();
+			}
+		}
+		[TestCase]
+		public void selectWithQualifiersOnly()
+		{
+			IndexTask task = this.client.CreateIndex(null, TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, "age_index", "age", IndexType.NUMERIC);
+			task.Wait();
+			queryEngine.refreshCluster();
+			Qualifier qual1 = new Qualifier("color", Qualifier.FilterOperation.EQ, Value.Get("green"));
+			Qualifier qual2 = new Qualifier("age", Qualifier.FilterOperation.BETWEEN, Value.Get(28), Value.Get(29));
+			KeyRecordEnumerator it = queryEngine.Select(TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, null, qual1, qual2);
+			try
+			{
+				while (it.MoveNext())
+				{
+					KeyRecord rec = it.Current;
+					Assert.Equals("green", rec.record.GetString("color"));
+					int age = rec.record.GetInt("age");
+					Assert.True(age >= 28 && age <= 29);
+				}
+			}
+			finally
+			{
+				it.Close();
+			}
+		}
+		[TestCase]
+		public void selectWithGeneration()
+		{
+			queryEngine.refreshCluster();
+			Qualifier qual1 = new GenerationQualifier(Qualifier.FilterOperation.GTEQ, Value.Get(1));
+			KeyRecordEnumerator it = queryEngine.Select(TestQueryEngine.NAMESPACE, TestQueryEngine.SET_NAME, null, qual1);
+			try
+			{
+				while (it.MoveNext())
+				{
+					KeyRecord rec = it.Current;
+					Assert.True(rec.record.generation >= 1);
+				}
+			}
+			finally
+			{
+				it.Close();
+			}
+		}
+
+
+	}
+}
